85f875711d724c2c2042b80ed8a38334e239e152
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-12-25 15:59:06 +0500

    Paranormal Investigating Website

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,7 @@ export const ORG_CONFIG = {
   name: "Dar ul Ruqyah",
   tagline: "Protection, Research & Healing Through Qur'an and Sunnah",
   phone: "+92 (316) 021-2457", // Added phone number
-  email: "darulrugyah@gmail.com", // Updated email
+  email: "contact.darulruqyah@gmail.com", // Updated email
   address: "123 Islamic Center Dr, City, State 12345", // Added address
   whatsapp: {
     number: "+923160212457",
